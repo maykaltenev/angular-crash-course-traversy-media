3c9339f8bce92286d2faaadaf10efab7f770d31e
Maykal Tenev <michaeltenev@gmail.com>
2023-04-29 13:01:54 +0200

    json server

--- angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -1,14 +1,18 @@
+import { TaskService } from './../../services/task.service';
 import { Component, OnInit } from '@angular/core';
+
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  constructor() {}
+  tasks: Task[] = [];
+  constructor(private TaskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.TaskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 }
