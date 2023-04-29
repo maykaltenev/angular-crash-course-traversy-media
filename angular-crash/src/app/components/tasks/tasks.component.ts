80696ee088e573955dc9dcf9c0657b9797295602
Maykal Tenev <michaeltenev@gmail.com>
2023-04-29 12:19:54 +0200

    task-item

--- angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-tasks';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS;
+  constructor() {}
 
+  ngOnInit(): void {}
 }
